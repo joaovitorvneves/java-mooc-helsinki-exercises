aa6a1eda4890820785e50a5e3c6716e7701f6ea8
unknown <joaovitorvneves@gmail.com>
2025-07-15 14:05:22 -0300

    Soluciona Part03_30: AgeOfTheOldest

---
+++ Java-Programming-I/Part03/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class AgeOfTheOldest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int oldest = 0;
+        
+        while (true) {
+            String text = scanner.nextLine();
+            
+            if (text.equals("")) {
+                break;
+            }
+            
+            String[] pieces = text.split(",");
+            
+            int age = Integer.valueOf(pieces[1]);
+            
+            if (oldest < age) {
+                oldest = age;
+            }
+        }
+        
+        System.out.println("Age of the oldest: " + oldest);
+    }
+}
