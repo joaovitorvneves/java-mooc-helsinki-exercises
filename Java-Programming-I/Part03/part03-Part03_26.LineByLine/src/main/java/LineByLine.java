eb34a720fd3dd965b15d4d2c61f769b26f469370
unknown <joaovitorvneves@gmail.com>
2025-07-15 14:05:18 -0300

    Soluciona Part03_26: LineByLine

---
+++ Java-Programming-I/Part03/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class LineByLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String text = scanner.nextLine();
+            
+            if (text.equals("")) {
+                return;
+            }
+            
+            String[] pieces = text.split(" ");
+            
+            for (int i = 0; i < pieces.length; ++i) {
+                System.out.println(pieces[i]);
+            }
+        }
+    }
+}
