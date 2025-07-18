2c0ac05b5a4543617be3c5043b8528c2f7086c4f
unknown <joaovitorvneves@gmail.com>
2025-07-18 16:26:46 -0300

    Soluciona Part07_02: LiquidContainers2

---
+++ Java-Programming-I/Part07/part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class LiquidContainers2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Container first = new Container();
+        Container second = new Container();
+        
+        while (true) {
+            System.out.println("First: " + first);
+            System.out.println("Second: " + second);
+
+            String input = scan.nextLine();
+            
+            if (input.equals("quit")) {
+                break;
+            }
+            
+            String[] pieces = input.split(" ");
+            
+            String command = pieces[0];
+            int value = Integer.valueOf(pieces[1]);
+            
+            if (command.equals("add")) {
+                first.add(value);
+            }
+            
+            if (command.equals("move")) {
+                if (first.contains() < value) {
+                    second.add(first.contains());
+                } else {
+                    second.add(value);
+                }
+                
+                first.remove(value);
+            }
+            
+            if (command.equals("remove")) {
+                second.remove(value);
+            }
+        }
+    }
+}
